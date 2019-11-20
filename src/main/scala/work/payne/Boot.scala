49414035915f4eace2ea54692bd92fb5303c2fa9
David Payne <4246575+davidwrpayne@users.noreply.github.com>
2019-11-19 20:22:03 -0800

    adding max consecutive sequence challenge

--- src/main/scala/work/payne/Boot.scala
+++ src/main/scala/work/payne/Boot.scala
@@ -1,12 +1,12 @@
 package work.payne
 
 import work.payne.ImageStuff.ImageManipulation
-import work.payne.challenges.{DynamicArray, FindNthSmallestNumber, HRSolveArrays, LRUCacheProblem}
+import work.payne.challenges.{DynamicArray, FindMaxConsecutiveSequence, FindNthSmallestNumber, HRSolveArrays, LRUCacheProblem}
 
 object Boot extends App with MyLogging {
 
 
-   new FindNthSmallestNumber().run()
+   new FindMaxConsecutiveSequence().run()
 
 }
 
