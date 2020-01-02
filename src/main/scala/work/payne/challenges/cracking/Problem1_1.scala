aa520dab6cbaf6f61e48a0ec073e16f57b25a6ae
David Payne <4246575+davidwrpayne@users.noreply.github.com>
2020-01-01 16:29:04 -0800

    Adding Problem 1.8 from Cracking the Coding Interview

--- src/main/scala/work/payne/challenges/cracking/Problem1_1.scala
+++ src/main/scala/work/payne/challenges/cracking/Problem1_1.scala
@@ -7,18 +7,27 @@ import scala.collection.mutable
 class Problem1_1 extends CodingProblem {
   /** Implement a function that determines if a string has all unique elements */
   /** What if you can't use an additional datastructure. */
-  override def run(): Unit = {
-    val unique = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-=_+/.,<>?"
-    val non_unique = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-=_+/.,<>?AA"
-    println(s"unique elements $unique = ${allUniqueElementsNoExtraDataStructure(unique)}")
-  }
 
   // initial thought is it set up a hashset of each character
 
   // for if we can't use extra information my thought is to loop but only loop over the remaining text as we've already
   // checked the earlier portion of text.
 
+
+  // Reading through the solution they point out that the HashSet of course can be implemented using array of booleans
+  // where the length of the array is equal to the character set size.  ASCII vs UTF-8
+  // Also worth mentioning is that you can check the lenght of the string against the character set size. if more then the
+  // set size you can return early.
+  override def run(): Unit = {
+    val unique = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-=_+/.,<>?"
+    val non_unique = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-=_+/.,<>?AA"
+    println(s"unique elements $unique = ${allUniqueElementsNoExtraDataStructure(unique)}")
+  }
+
+
+  /** This can be improved by early return and different data structure */
   def allUniqueElements(input: String): Boolean = {
+
     val seen = mutable.HashSet[Char]()
     input.foldLeft(true) {
       case (false, _) => false
